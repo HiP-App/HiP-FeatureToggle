bb0520de012daec8be68c6a717597d22c126b560
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2017-03-27 23:38:17 +0200

    Add a dbContext class (empty for now)

---
+++ HiP-FeatureToggle/Data/ToggleDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace de.uni_paderborn.si_lab.hip.featuretoggle.data
+{
+    public class ToggleDbContext: DbContext
+    {
+        /*
+         * TODO: add DbSets
+         * Example:
+         * public DbSet<AssociatedTopic> AssociatedTopics { get; set; }
+         */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*
+             * TODO: create any mappings that you need
+             * Example:
+             * modelBuilder.Entity<User>().HasIndex(b => b.Email).IsUnique();
+             * new AssociatedTopicMap(modelBuilder.Entity<AssociatedTopic>());
+             */
+        }
+    }
+}
