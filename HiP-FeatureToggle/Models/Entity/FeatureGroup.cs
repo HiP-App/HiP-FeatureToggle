905b004b85531dfa02bc658ad877f31df15a5a32
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-04-05 18:57:36 +0200

    Added entity classes and DB migration

---
+++ HiP-FeatureToggle/Models/Entity/FeatureGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Models.Entity
+{
+    public class FeatureGroup
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<User> Members { get; set; }
+
+        /// <summary>
+        /// The features that are enabled in this feature group.
+        /// The opposite direction of this many-to-many relation is <see cref="Feature.GroupsWhereEnabled"/>.
+        /// </summary>
+        public IList<FeatureToFeatureGroupMapping> EnabledFeatures { get; set; }
+    }
+}
---
+++ HiP-FeatureToggle/Models/Entity/FeatureToFeatureGroupMapping.cs
@@ -0,0 +1,31 @@
+﻿namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Models.Entity
+{
+    /// <remarks>
+    /// Since EF Core does not (yet) support many-to-many relationships, the relationship between features
+    /// and feature groups is modelled as two 1-to-many relationships. In other words, this class acts as
+    /// the join table.
+    ///
+    /// [Feature]----------------[FeatureToFeatureGroupMapping]-------------[FeatureGroup]
+    ///          1              n                              m           1
+    /// </remarks>
+    public class FeatureToFeatureGroupMapping
+    {
+        public Feature Feature { get; set; }
+
+        public int FeatureId { get; set; }
+
+        public FeatureGroup Group { get; set; }
+
+        public int GroupId { get; set; }
+
+        public FeatureToFeatureGroupMapping()
+        {
+        }
+
+        public FeatureToFeatureGroupMapping(Feature feature, FeatureGroup group)
+        {
+            Feature = feature;
+            Group = group;
+        }
+    }
+}
