a949b46343f2909e330110393234da724739f8b1
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-06-06 22:29:39 +0200

    Bug fixes to temporarily disable auth

--- HiP-FeatureToggle/Services/CmsService.cs
+++ HiP-FeatureToggle/Services/CmsService.cs
@@ -1,10 +1,12 @@
-﻿namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Services
+﻿using System.Security.Claims;
+
+namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Services
 {
     /// <summary>
     /// Dummy service that fakes authorization.
     /// </summary>
     public class CmsService
     {
-        public string GetUserRole(string identity) => "Administrator";
+        public string GetUserRole(ClaimsPrincipal user) => "Administrator";
     }
 }
