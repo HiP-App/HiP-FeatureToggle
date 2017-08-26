1072b317271e28eb44a1616c75fe3bf80c631767
nagaraj1993 <nagaraj@mail.uni-paderborn.de>
2017-08-26 13:43:06 +0200

    HIPCMS-666-Added-Check-For-NullReferenceException-Due-To-Resharper
    Resharper check in TFS fails and hence build is getting partially
    succeeded. I don't know why this error suddenly came so wierd which did
    not come up in the previous commits. So I added the check. Please check
    the details in the commit for the reason to add the Exception check.

--- HiP-FeatureToggle/Controllers/FeaturesController.cs
+++ HiP-FeatureToggle/Controllers/FeaturesController.cs
@@ -161,6 +161,10 @@ namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Controllers
             {
                 return StatusCode(409, e.Message); // invalid parent modification
             }
+            catch (NullReferenceException e)
+            {
+                return NotFound(e.Message); // feature to be updated does not exist
+            }
         }
 
         /// <summary>
