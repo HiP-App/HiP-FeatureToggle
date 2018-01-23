c88427d0730c4f46849efa8a1334c752cf6fa3ee
Michél Burkhardt <mburkhardt@live.de>
2018-01-24 00:12:58 +0100

    Revert changes

--- HiP-FeatureToggle/Program.cs
+++ HiP-FeatureToggle/Program.cs
@@ -8,15 +8,10 @@ namespace PaderbornUniversity.SILab.Hip.FeatureToggle
     {
         public static void Main(string[] args)
         {
-            var configurationBuilder = new ConfigurationBuilder()
-                .AddEnvironmentVariables().Build();
-
-            var port = configurationBuilder.GetValue<string>("Config:Port");
-
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls(string.IsNullOrEmpty(port) ? "http://*:5000" : $"http://*:{port}")
+                .UseUrls("http://*:60000")
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
