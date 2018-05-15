cf6624d4d8999f0509b22390a34712cc0baa7aa7
thielep <thielep@mail.uni-paderborn.de>
2018-05-15 17:05:13 +0200

    Removed unnecessary using

--- HiP-FeatureToggle/Controllers/FeatureGroupsController.cs
+++ HiP-FeatureToggle/Controllers/FeatureGroupsController.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
-using Newtonsoft.Json;
 
 namespace PaderbornUniversity.SILab.Hip.FeatureToggle.Controllers
 {
