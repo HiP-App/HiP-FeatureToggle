b0c7d7ea8506be87ca24dc9cc040305242c7fb93
Michél Burkhardt <mburkhardt@live.de>
2018-08-20 13:37:15 +0200

    Added FeatureToggleService to SDK (#42)
    * Added FeatureToggleService to SDK
    
    * Updated dependencies

---
+++ HiP-FeatureToggle.Sdk/FeatureToggleConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace PaderbornUniversity.SILab.Hip.FeatureToggle
+{
+    /// <summary>
+    /// Configuration properties for clients using the FeatureToggle SDK.
+    /// </summary>
+    public sealed class FeatureToggleConfig
+    {
+        /// <summary>
+        /// URL pointing to a running instance of the FeatureToggle service.
+        /// Example: "https://docker-hip.cs.upb.de/develop/feature-toggle"
+        /// </summary>
+        public string FeatureToggleHost { get; set; }
+    }
+}
