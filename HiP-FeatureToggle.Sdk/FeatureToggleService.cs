4ac81e0d90d923c31b85e807b30e002a10d71880
Michél Burkhardt <mburkhardt@live.de>
2018-08-20 13:56:14 +0200

    Fixed wrong class

--- HiP-FeatureToggle.Sdk/FeatureToggleService.cs
+++ HiP-FeatureToggle.Sdk/FeatureToggleService.cs
@@ -4,41 +4,40 @@ using Microsoft.Extensions.Options;
 
 namespace PaderbornUniversity.SILab.Hip.FeatureToggle
 {
+    /// <summary>
+    /// A service that can be used with ASP.NET Core dependency injection.
+    /// Usage: In ConfigureServices():
+    /// <code>
+    /// services.Configure&lt;FeatureToggleConfig&gt;(Configuration.GetSection("Endpoints"));
+    /// services.AddSingleton&lt;FeatureToggleService&gt;();
+    /// </code>
+    /// </summary>
     public class FeatureToggleService
     {
-        /// <summary>
-        /// A service that can be used with ASP.NET Core dependency injection.
-        /// Usage: In ConfigureServices():
-        /// <code>
-        /// services.Configure&lt;FeatureToggleConfig&gt;(Configuration.GetSection("Endpoints"));
-        /// services.AddSingleton&lt;FeatureToggleService&gt;();
-        /// </code>
-        /// </summary>
-        public class DataStoreService
+
+        private readonly FeatureToggleConfig _config;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FeatureToggleService(IOptions<FeatureToggleConfig> config, ILogger<FeatureToggleService> logger,
+            IHttpContextAccessor httpContextAccessor)
         {
-            private readonly FeatureToggleConfig _config;
-            private readonly IHttpContextAccessor _httpContextAccessor;
+            _config = config.Value;
+            _httpContextAccessor = httpContextAccessor;
 
-            public DataStoreService(IOptions<FeatureToggleConfig> config, ILogger<FeatureToggleService> logger,
-                IHttpContextAccessor httpContextAccessor)
-            {
-                _config = config.Value;
-                _httpContextAccessor = httpContextAccessor;
-
-                if (string.IsNullOrWhiteSpace(config.Value.FeatureToggleHost))
-                    logger.LogWarning($"{nameof(FeatureToggleConfig.FeatureToggleHost)} is not configured correctly!");
-            }
-
-            public FeaturesClient Features => new FeaturesClient(_config.FeatureToggleHost)
-            {
-                Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
-            };
-
-            public FeatureGroupsClient FeatureGroups => new FeatureGroupsClient(_config.FeatureToggleHost)
-            {
-                Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
-            };
-
+            if (string.IsNullOrWhiteSpace(config.Value.FeatureToggleHost))
+                logger.LogWarning($"{nameof(FeatureToggleConfig.FeatureToggleHost)} is not configured correctly!");
         }
+
+        public FeaturesClient Features => new FeaturesClient(_config.FeatureToggleHost)
+        {
+            Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
+        };
+
+        public FeatureGroupsClient FeatureGroups => new FeatureGroupsClient(_config.FeatureToggleHost)
+        {
+            Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
+        };
+
     }
 }
+
