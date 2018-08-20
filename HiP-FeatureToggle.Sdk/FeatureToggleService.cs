dedbe09539ff32a50fbd0be3a3104aee301b0522
Michél Burkhardt <mburkhardt@live.de>
2018-08-20 13:14:55 +0200

    Added FeatureToggleService to SDK

---
+++ HiP-FeatureToggle.Sdk/FeatureToggleService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace PaderbornUniversity.SILab.Hip.FeatureToggle
+{
+    public class FeatureToggleService
+    {
+        /// <summary>
+        /// A service that can be used with ASP.NET Core dependency injection.
+        /// Usage: In ConfigureServices():
+        /// <code>
+        /// services.Configure&lt;FeatureToggleConfig&gt;(Configuration.GetSection("Endpoints"));
+        /// services.AddSingleton&lt;FeatureToggleService&gt;();
+        /// </code>
+        /// </summary>
+        public class DataStoreService
+        {
+            private readonly FeatureToggleConfig _config;
+            private readonly IHttpContextAccessor _httpContextAccessor;
+
+            public DataStoreService(IOptions<FeatureToggleConfig> config, ILogger<FeatureToggleService> logger,
+                IHttpContextAccessor httpContextAccessor)
+            {
+                _config = config.Value;
+                _httpContextAccessor = httpContextAccessor;
+
+                if (string.IsNullOrWhiteSpace(config.Value.FeatureToggleHost))
+                    logger.LogWarning($"{nameof(FeatureToggleConfig.FeatureToggleHost)} is not configured correctly!");
+            }
+
+            public FeaturesClient Features => new FeaturesClient(_config.FeatureToggleHost)
+            {
+                Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
+            };
+
+            public FeatureGroupsClient FeatureGroups => new FeatureGroupsClient(_config.FeatureToggleHost)
+            {
+                Authorization = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"]
+            };
+
+        }
+    }
+}
